a4da607c847e41512107e5dbf892f920b51c8879
E Tham <emintham@gmail.com>
2013-06-06 05:15:15 -0400

    fixes messages and navigation layouts; adds sessions and registration views; adds more fields to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :user_id, :name, :affiliation, :specialisation,
+     :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  
+  validates :user_id,
+     :uniqueness => true,
+     :presence => { :on => :create },
+     :exclusion => { :in => %w( admin superuser administrator root ),
+        :message => "Nice try..." }
+
+  validates :email,
+     :uniqueness => { :allow_blank => true },
+     :format => { :with => /@/, :on => :create, :allow_blank => true }
+
 end
