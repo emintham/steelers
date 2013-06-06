09c0e9a729564de3563f1710ac1982c3b54aa0aa
E Tham <emintham@gmail.com>
2013-06-06 04:40:41 -0400

    migrates db and prepares test db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130606083548) do
+ActiveRecord::Schema.define(:version => 20130606083938) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -37,6 +37,10 @@ ActiveRecord::Schema.define(:version => 20130606083548) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "user_id"
+    t.string   "affiliation"
+    t.string   "specialisation"
+    t.integer  "num_jobs"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
