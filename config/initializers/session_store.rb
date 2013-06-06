fc3a418f0d150fbb6d4890bc7fbc9de55e89b99b
E Tham <emintham@gmail.com>
2013-06-06 02:03:25 -0400

    first commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Steelers::Application.config.session_store :cookie_store, key: '_Steelers_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Steelers::Application.config.session_store :active_record_store
