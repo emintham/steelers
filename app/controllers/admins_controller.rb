b92464643e80b5504b0ff3074e5edaca24c0a09e
E Tham <emintham@gmail.com>
2013-08-16 16:46:08 -0400

    security patch

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,6 +2,8 @@ class AdminsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    authorize! :index, @user, :message => 'Not authorized as administrator.'
+
     @users = User.all
     @user = current_user
     @servers = Server.all
