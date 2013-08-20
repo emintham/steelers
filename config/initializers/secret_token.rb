7ab79d2a6575ad7558dbdf04d5407f5a9a47255d
E Tham <emintham@gmail.com>
2013-08-20 18:21:27 -0400

    rm secret token

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Steelers::Application.config.secret_token = '53ff7693bab7e18a9fe329c467073fcdf909e3a3e804587ffea4cc7b424126c2b33b2ce6fa202471e72404792ddd6cc55b2542054fa29aac800fc4482517d088'
